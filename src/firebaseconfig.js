9ada202b145c8a690d12609351e14a1c3a38f5e1
ShreeshaM07 <shreesham2k22@yahoo.com>
2024-01-08 21:23:24 +0530

    register and login page without the variables going to firebase

---
+++ src/firebaseconfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAB1_zay9AIcEv6AyyJ-PWzTJKB4uZLUYQ",
+    authDomain: "eco-footprint-bc1ab.firebaseapp.com",
+    projectId: "eco-footprint-bc1ab",
+    storageBucket: "eco-footprint-bc1ab.appspot.com",
+    messagingSenderId: "71979483983",
+    appId: "1:71979483983:web:547bde7626b67099d97b3a"
+  };
+
+export const app = initializeApp(firebaseConfig);
