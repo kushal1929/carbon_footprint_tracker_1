351f865a333fdc8ebb93a8240cc536cd8595b0a9
ShreeshaM07 <shreesham2k22@yahoo.com>
2024-01-09 16:43:35 +0530

    changed firebase email

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAB1_zay9AIcEv6AyyJ-PWzTJKB4uZLUYQ",
-    authDomain: "eco-footprint-bc1ab.firebaseapp.com",
-    projectId: "eco-footprint-bc1ab",
-    storageBucket: "eco-footprint-bc1ab.appspot.com",
-    messagingSenderId: "71979483983",
-    appId: "1:71979483983:web:547bde7626b67099d97b3a"
-  };
+  apiKey: "AIzaSyCR_zM2ARzxCx03U3Sz-zzKpLOAGCXs-QQ",
+  authDomain: "eco-footprint-tracker.firebaseapp.com",
+  projectId: "eco-footprint-tracker",
+  storageBucket: "eco-footprint-tracker.appspot.com",
+  messagingSenderId: "620362634074",
+  appId: "1:620362634074:web:c3f3ed7905d616a0749963"
+};
 
 export const app = initializeApp(firebaseConfig);
