b8c708d63f35efa6353cd144a1f78befd9a1221c
Abhay-Lejith <abhaylejith@gmail.com>
2024-01-13 14:55:29 +0530

    tailwind works somehow

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/common/Form.js",
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
