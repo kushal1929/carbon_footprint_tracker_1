463c3cdfd510cc3955036f166bbdb0465609a2b6
ShishirAshok <144123572+ShishirAshok@users.noreply.github.com>
2024-01-24 12:29:47 +0530

    Update Header.js

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -121,6 +121,9 @@ const handleLogout = () => {
           <a href="/eating-habits" className="text-sm font-semibold leading-6 text-gray-900">
             Questionnaire
           </a>
+          <a href="/Quiz" className="text-sm font-semibold leading-6 text-gray-900">
+            Quiz
+          </a>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <button onClick={handleLogout} className="text-sm font-semibold leading-6 text-gray-900">
@@ -195,7 +198,14 @@ const handleLogout = () => {
                 >
                   Questionare
                 </a>
+                <a
+                  href="/Quiz"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Quiz
+                </a>
               </div>
+              
               <div className="py-6">
                 <a
                   href="/"
@@ -211,3 +221,6 @@ const handleLogout = () => {
     </header>
   )
 }
+
+
+//Added a button for quiz line-124,201
