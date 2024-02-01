f9b46a3db5156ac4ed690c8f5969ae76c8173195
ShreeshaM07 <shreesham2k22@yahoo.com>
2024-02-02 00:13:32 +0530

    styled the quiz page

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -184,15 +184,15 @@ const QuizApp = ({ app }) => {
   };
 
   const renderQuizContent = () => (
-    <div className="flex flex-col items-center">
-      <h3 className="text-2xl mb-4">{`Question ${currentQuestion + 1}`}</h3>
-      <p className="mb-8">{questions[currentQuestion]?.Question}</p>
+    <div className="mt-[15%] mb-[15%] flex flex-col items-center">
+      <h3 className="text-center text-indigo-800 text-opacity-50 text-3xl mt-[5%] mr-[5%] ml-[5%] mb-4 md:text-5xl">{`Question ${currentQuestion + 1}`}</h3>
+      <p className="text-center text-2xl mr-[5%] ml-[5%] mt-[7%] mb-8 md:text-3xl">{questions[currentQuestion]?.Question}</p>
       
-      <div className="flex justify-center space-x-4">
+      {/* <div className="flex justify-center space-x-4"> */}
         {["Option1", "Option2"].map((option, index) => (
           <div
             key={index}
-            className={`bg-gray-300 p-4 rounded-md ${
+            className={`text-center w-[60%] mx-auto mt-[2%] mb-[5%] border-amber-300 border-2 p-4 rounded-md hover:bg-amber-400 ${
               selectedAnswer !== null &&
               (selectedAnswer === questions[currentQuestion]?.Correct_Answer
                 ? "bg-green-300"
@@ -208,7 +208,7 @@ const QuizApp = ({ app }) => {
             </button>
           </div>
         ))}
-      </div>
+      {/* </div> */}
 
       {selectedAnswer !== null && (
         <div className="mt-4">
@@ -230,24 +230,27 @@ const QuizApp = ({ app }) => {
 
   const renderQuizCompleted = () => (
     <div className="flex flex-col items-center">
-      <h3 className="text-2xl">Quiz Completed</h3>
-      <p>Your final score is: {score} out of {questions.length}</p>
+      <h3 className="text-center text-indigo-800 text-opacity-50 text-3xl mt-[5%] mr-[5%] ml-[5%] mb-4 md:text-5xl">Quiz Completed</h3>
+      <p className='text-center text-2xl mr-[5%] ml-[5%] mt-[7%] mb-8 md:text-3xl'>Your final score is: {score} out of {questions.length}</p>
     </div>
   );
 
   return (
-    <>
-    <Header/>
-    <div>
-      {loading ? (
-        <p>Loading questions...</p>
-      ) : quizCompleted ? (
-        renderQuizCompleted()
-      ) : (
-        renderQuizContent()
-      )}
+    <div className='w-full h-full bg-gradient-to-r from-yellow-300 via-lime-300 to-green-300'>
+      <Header/>
+      <div className='mt-[10%]'>
+        <div className='mx-[5%] sm:mx-[15%] rounded bg-white'>
+          {loading ? (
+            <p>Loading questions...</p>
+          ) : quizCompleted ? (
+            renderQuizCompleted()
+          ) : (
+            renderQuizContent()
+          )}
+        </div>
+      </div>
+      
     </div>
-    </>
   );
 };
 
