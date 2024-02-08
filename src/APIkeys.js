cb29db1d3145d38503bcec5a57cfe3a2933b51a8
ShishirAshok <shishirashok@gmail.com>
2024-02-08 22:42:08 +0530

    removed console statements

---
+++ src/APIkeys.js
@@ -0,0 +1,7 @@
+//Gemini
+const geminiAPIkey = 'AIzaSyDSpdvDXV4b-L6j3RrNvfkfOVmEf2Rbq3E';
+export {geminiAPIkey};
+
+//Maps
+const maps_api= `https://maps.googleapis.com/maps/api/js?key=AIzaSyAmFmIBoCAU5enhOZew7ijQYYUGnTabOOg&libraries=places&callback=initialize`;
+export {maps_api};
