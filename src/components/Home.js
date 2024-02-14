a80a6c7665f9a14bdd7ab66c5dc67d9b51770653
ShreeshaM07 <shreesham2k22@yahoo.com>
2024-02-15 00:35:44 +0530

    centered boxes

--- src/components/Home.js
+++ src/components/Home.js
@@ -388,7 +388,7 @@ export default function Home() {
       </div>
 
     <div className="flex flex-wrap flex-row gap-y-3 mb-2 w-full lg:flex-nowrap gap-x-3 px-3 place-content-evenly sm:place-content-evenly  items-stretch justify-stretch mt-5 720px:mt-[20%] lg:mt-[28%] xl:mt-[4%] 2xl:mt-[2.5%]">
-      <div className="bg-amber-600 flex flex-row place-content-evenly mx-auto w-[80%] block rounded-xl border border-gray-200 p-4 sm:p-8 shadow-xl transition hover:border-lime-300/10 hover:shadow-amber-700/50 ">
+      <div className="items-center bg-amber-600 flex flex-row place-content-evenly mx-auto w-[80%] block rounded-xl border border-gray-200 p-4 sm:p-8 shadow-xl transition hover:border-lime-300/10 hover:shadow-amber-700/50 ">
         <a href="/consumption-data">
           <p className="text-md sm:text-2xl text-white animate-pulse">
           Fill in the calculator to get detailed analytics and a personalized action plan!
@@ -396,14 +396,14 @@ export default function Home() {
         </a>
         <BsCalculator className="text-white h-full w-auto"/>
       </div>
-      <div className="bg-amber-500 flex flex-col place-content-evenly mx-auto w-[80%] block rounded-xl border border-gray-200 p-4 sm:p-8 shadow-xl transition hover:border-lime-300/10 hover:shadow-amber-600/50 ">
+      <div className="items-center bg-amber-500 flex flex-col place-content-evenly mx-auto w-[80%] block rounded-xl border border-gray-200 p-4 sm:p-8 shadow-xl transition hover:border-lime-300/10 hover:shadow-amber-600/50 ">
         <a href="/consumption-data">
           <p className="text-lg sm:text-2xl text-white ">
            Read our blog to understand what carbon footprint is and how it affects the world         
           </p>
         </a>
       </div>
-      <div className="bg-amber-400 flex flex-row place-content-evenly mx-auto w-[80%] block rounded-xl border border-gray-200 p-8 shadow-xl transition hover:border-lime-300/10 hover:shadow-amber-500/50 ">
+      <div className="items-center bg-amber-400 flex flex-row place-content-evenly mx-auto w-[80%] block rounded-xl border border-gray-200 p-8 shadow-xl transition hover:border-lime-300/10 hover:shadow-amber-500/50 ">
           <a href="/share-score">
             <p className="text-lg sm:text-2xl text-white ">
               Download,Share score on social media        
